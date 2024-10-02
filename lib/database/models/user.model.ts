229d5e2300b78f127334ad1ca6a886138d68e9c6
unknown <natnaelm552@gmail.com>
2024-10-02 18:11:21 +0300

    mongo connection and models

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,16 @@
+import { model, models, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  clerkId: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  planId: { type: Number, default: 1 },
+  creditBalance: { type: Number, default: 20 },
+});
+
+const User = models?.User || model("User", UserSchema);
+
+export default User;
