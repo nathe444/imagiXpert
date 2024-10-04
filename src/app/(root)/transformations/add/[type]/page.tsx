c482668677736af6f7a47fcc9b16749728c21322
unknown <natnaelm552@gmail.com>
2024-10-04 17:23:52 +0300

    aspect ratio for fill page

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,28 @@
 import Header from "@/components/shared/Header";
 import React from "react";
 import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
   const transformation = transformationTypes[type];
   return (
-    <Header title={transformation.title} subtitle={transformation.subTitle} />
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
   );
 };
 
